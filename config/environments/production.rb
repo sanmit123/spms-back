cde21af9fd52283f0675d132d3b207b227910175
sanmit123 <sanmitvartak.sv@gmail.com>
2024-01-04 21:01:33 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Allow requests from any host
+  config.hosts << /[a-z0-9]+\.onrender\.com/
+
+  
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
